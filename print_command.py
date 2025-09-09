3cf83cb197bc444f9cb848e95ba05c27af7c239c
Your GitHub Username <liem20k@gmail.com>
2025-09-09 21:44:33 +0700

    09-09 21:44, mb, update print

---
+++ print_command.py
@@ -0,0 +1,13 @@
+print("hello world")
+"""
+Nộidung video 4 giới thiệu print
+"""
+print(5)
+
+print(5+3)
+print("5+3 =", 5+3)
+print(6); print("hello 6 world")
+print("xin", "chao", "các", "bạn", sep="-")
+# kết thúc :
+print("xin", "chào", end=' ')
+print("Liêm")
